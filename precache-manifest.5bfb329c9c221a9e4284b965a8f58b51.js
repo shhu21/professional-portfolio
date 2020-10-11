e04343438e9f92836d8934f8e6dc230efd3295eb
shhu21 <hu.sara21@gmail.com>
2020-10-10 23:57:39 -0700

    Updates

---
+++ precache-manifest.5bfb329c9c221a9e4284b965a8f58b51.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f6855c34ddbaeeafd8927f7e9bbecba",
+    "url": "/professional-portfolio/index.html"
+  },
+  {
+    "revision": "28588f626afa01c8a8ac",
+    "url": "/professional-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "17eff92a896f9171857c",
+    "url": "/professional-portfolio/static/css/main.57474cfa.chunk.css"
+  },
+  {
+    "revision": "28588f626afa01c8a8ac",
+    "url": "/professional-portfolio/static/js/2.1fe31f2a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/professional-portfolio/static/js/2.1fe31f2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17eff92a896f9171857c",
+    "url": "/professional-portfolio/static/js/main.8acbc463.chunk.js"
+  },
+  {
+    "revision": "72f7aae8c9dffe1e6e53",
+    "url": "/professional-portfolio/static/js/runtime-main.5a9c1606.js"
+  },
+  {
+    "revision": "4637554c66df6c557f80ed3efb4e06cb",
+    "url": "/professional-portfolio/static/media/CampusMate (Android).4637554c.png"
+  },
+  {
+    "revision": "ea09985b797995e6c2fa763e3d1fd0dd",
+    "url": "/professional-portfolio/static/media/CampusMate (IOS).ea09985b.png"
+  },
+  {
+    "revision": "87814ee21424f82006444be0e3ecbccf",
+    "url": "/professional-portfolio/static/media/Instagram Image Gallery.87814ee2.png"
+  },
+  {
+    "revision": "482a0534064250822f97031aab6cb024",
+    "url": "/professional-portfolio/static/media/Portfolio.482a0534.png"
+  },
+  {
+    "revision": "156205b94e858260c42619e55eca7e8d",
+    "url": "/professional-portfolio/static/media/Pwd Generator.156205b9.png"
+  },
+  {
+    "revision": "aff65d5d8aa8c5444bb56823b12c0458",
+    "url": "/professional-portfolio/static/media/Scheduler.aff65d5d.png"
+  },
+  {
+    "revision": "e614c1ff064010e3e80db397ccac3e21",
+    "url": "/professional-portfolio/static/media/Team Profile Generator.e614c1ff.png"
+  },
+  {
+    "revision": "03f8f05ab0676cb7cdcd86ec1d1ecbfe",
+    "url": "/professional-portfolio/static/media/Trip Planner.03f8f05a.png"
+  },
+  {
+    "revision": "4972b0cf9a959840923a9825987dd293",
+    "url": "/professional-portfolio/static/media/Weather Dashboard.4972b0cf.png"
+  },
+  {
+    "revision": "e869fc8c934965f3244c986368a6723d",
+    "url": "/professional-portfolio/static/media/Wine Blogger.e869fc8c.png"
+  },
+  {
+    "revision": "0a55d60226eda52413bf58b5314f8076",
+    "url": "/professional-portfolio/static/media/background.0a55d602.png"
+  },
+  {
+    "revision": "99f4634b2cc5701779e077f6cf890d0e",
+    "url": "/professional-portfolio/static/media/background.99f4634b.jpg"
+  },
+  {
+    "revision": "6de530c6397570a1427bffc34e372fef",
+    "url": "/professional-portfolio/static/media/profile.6de530c6.jpeg"
+  }
+]);
